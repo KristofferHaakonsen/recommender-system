907e59d73286754b1b2617e430cae51dd304387f
KristofferHaakonsen <kristofferhhaakonsen@gmail.com>
2021-03-26 18:57:49 +0100

    Add evaluation

--- evaluation.py
+++ evaluation.py
@@ -1,21 +1,27 @@
-def evaluate(pred, actual, k, num_requests=1, mse = 0.5):
+def evaluate(pred, actual, k, num_requests):
     """
-    Evaluate recommendations according to recall@k and ARHR@k
+    Evaluate recommendations according to recall@k, ARHR@k, and 
     k: top-k
+    num_request: The number of requests for predictions
     """
-    # TODO: Add num_requests
-    # TODO: Add mse
     total_num = len(actual)
     tp = 0.
     arhr = 0.
-    for p, t in zip(pred, actual):
-        if t in p:
-            tp += 1.
-            arhr += 1. / float(p.index(t) + 1.)
+    
+    for element in pred:
+        pred_articles = element.get("articles").tolist()
+        for p, t in zip(pred_articles, actual):
+            if t in p:
+                tp += 1.
+                arhr += 1. / float(pred_articles.index(t) + 1.)
+                
+
     recall = tp / float(total_num)
     arhr = arhr / len(actual)
     ctr = tp / num_requests * 100
     print("Recall@{} is {:.4f}".format(k, recall))
     print("ARHR@{} is {:.4f}".format(k, arhr))
     print("CTR@{} is {}%".format(k, ctr))
-    print("MSE@{} is {}%".format(k, mse))
+
+
+
