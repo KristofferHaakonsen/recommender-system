960a73bad352abacc7180ede78c094d61ffd5347
Jonas Rødningen <jonasrod@stud.ntnu.no>
2021-04-09 19:02:57 +0200

    removed evalutation repetition

--- evaluation.py
+++ evaluation.py
@@ -1,7 +1,7 @@
-def evaluate_top_k(pred, actual, num_requests, k):
+def evaluate(pred, actual, num_requests, k_limit=None):
     """
     Evaluate recommendations according to recall@k, ARHR@k, and CTR@k
-    k: top-k
+    k_limit: limit number of articles to recommend. None=no limit
     num_request: The number of requests for predictions
     """
 
@@ -12,7 +12,8 @@ def evaluate_top_k(pred, actual, num_requests, k):
     # element represents a user and contains it's recommended articles
     for element in pred:
         pred_articles = element.get("articles").tolist()
-        pred_articles = pred_articles[:k]
+        if k_limit:
+            pred_articles = pred_articles[:k_limit]
         for p, t in zip(pred_articles, actual):
             if t in p:
                 tp += 1.
@@ -22,32 +23,10 @@ def evaluate_top_k(pred, actual, num_requests, k):
     arhr = arhr / len(actual)
     ctr = tp / num_requests * 100
 
-    print("\nRecall@{} is {:.4f}".format(k, recall))
-    print("ARHR@{} is {:.4f}".format(k, arhr))
-    print("CTR@{} is {}%".format(k, ctr))
-
-
-def evaluate(pred, actual, num_requests):
-    """
-    Evaluate recommendations according to recall, ARHR, and CTR
-    num_request: The number of requests for predictions
-    """
-
-    total_num = len(actual)
-    tp = 0.
-    arhr = 0.
-
-    for element in pred:
-        pred_articles = element.get("articles").tolist()
-        for p, t in zip(pred_articles, actual):
-            if t in p:
-                tp += 1.
-                arhr += 1. / float(pred_articles.index(t) + 1.)
-
-    recall = tp / float(total_num)
-    arhr = arhr / len(actual)
-    ctr = tp / num_requests * 100
-
-    print("\nRecall is {:.4f}".format(recall))
-    print("ARHR is {:.4f}".format(arhr))
-    print("CTR is {}%".format(ctr))
+    prefix = '@'
+    if not k_limit:
+        k_limit = ''
+        prefix = ''
+    print("\nRecall{} is {:.4f}".format(prefix + str(k_limit), recall))
+    print("ARHR{} is {:.4f}".format(prefix + str(k_limit), arhr))
+    print("CTR{} is {}%".format(prefix + str(k_limit), ctr))
