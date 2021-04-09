5d74015006a1551baab26297368a1f9c834d1c94
Jonas Rødningen <jonasrod@stud.ntnu.no>
2021-04-09 21:58:27 +0200

    fixed TP bug and edited CTR + recall metrics

--- evaluation.py
+++ evaluation.py
@@ -1,32 +1,44 @@
-def evaluate(pred, actual, num_requests, k_limit=None):
+def evaluate(pred, actual, k_limit=None):
     """
     Evaluate recommendations according to recall@k, ARHR@k, and CTR@k
     k_limit: limit number of articles to recommend. None=no limit
     num_request: The number of requests for predictions
     """
 
-    total_num = len(actual)
-    tp = 0.
+    num_users = len(actual)
+    num_requests = 0
+    tp_global = 0.
     arhr = 0.
+    recalls = []
 
     # element represents a user and contains it's recommended articles
     for element in pred:
         pred_articles = element.get("articles").tolist()
         if k_limit:
             pred_articles = pred_articles[:k_limit]
-        for p, t in zip(pred_articles, actual):
-            if t in p:
-                tp += 1.
-                arhr += 1. / float(pred_articles.index(t) + 1.)
+        actual_series = actual.loc[element["user_id"]]
+        # test_set_positives_list is a list of all test-set entries with value = 1.0 for this user
+        test_set_positives_list = list(actual_series[actual_series == 1].index)
+        tp_user = 0
+        for ind, pred in enumerate(pred_articles):
+            num_requests += 1
+            if pred in test_set_positives_list:
+                # the prediction/suggestion is a true positive (tp)
+                tp_global += 1.
+                tp_user += 1
+                arhr += 1. / float(ind + 1.)
+        user_recall = tp_user / len(test_set_positives_list)
+        recalls.append(user_recall)
 
-    recall = tp / float(total_num)
-    arhr = arhr / len(actual)
-    ctr = tp / num_requests * 100
+    recall_global = sum(recalls) / num_users
+    arhr = arhr / num_users
+    ctr = tp_global / num_requests * 100
 
     prefix = '@'
     if not k_limit:
         k_limit = ''
         prefix = ''
-    print("\nRecall{} is {:.4f}".format(prefix + str(k_limit), recall))
+    print("\nGlobal recall{} is {:.4f}".format(
+        prefix + str(k_limit), recall_global))
     print("ARHR{} is {:.4f}".format(prefix + str(k_limit), arhr))
-    print("CTR{} is {}%".format(prefix + str(k_limit), ctr))
+    print("CTR{} is {:.4f}%".format(prefix + str(k_limit), ctr))
